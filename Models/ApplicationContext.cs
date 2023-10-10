f0511c901f90a73730d2dea285369ca2b2ae07fe
Felskimd <gilmanshin.roman@yandex.ru>
2023-10-10 08:23:45 +0300

    Добавьте файлы проекта.

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFilms.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<string> Users { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
