3cb18a96196a395c2eddb3f34c94e4c838cfa8a6
Felskimd <gilmanshin.roman@yandex.ru>
2023-10-11 08:14:16 +0300

    Worlin' hard

---
+++ Models/APITicketProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyFilms.Models
+{
+    public class APITicketProvider
+    {
+        private readonly IConfiguration configuration;
+
+        public APITicketProvider(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GetTicket() => configuration["APITicket"];
+    }
+}
